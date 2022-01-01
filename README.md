aa6f02299c7d8e5cc3e2b6610a917c467b0b8b29
Marclus <marcluslopes@gmail.com>
2022-01-01 23:31:58 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# share_me_react" 
---
+++ shareme_frontend/src/container/Home.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useRef, useEffect } from 'react'
+import { HiMenu } from 'react-icons/hi'
+import { AiFillCloseCircle } from 'react-icons/ai'
+import { Link, Route, Routes } from 'react-router-dom'
+
+import { Sidebar, UserProfile } from '../components/index'
+
+const Home = () => {
+    return (
+        <div>
+            Home
+        </div>
+    )
+}
+
+export default Home
