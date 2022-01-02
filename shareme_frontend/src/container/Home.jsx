137324a8da3dc110364f3f0e154541f84ac09069
Marclus <marcluslopes@gmail.com>
2022-01-02 14:14:39 +0530

    removed env file

--- shareme_frontend/src/container/Home.jsx
+++ shareme_frontend/src/container/Home.jsx
@@ -4,11 +4,61 @@ import { AiFillCloseCircle } from 'react-icons/ai'
 import { Link, Route, Routes } from 'react-router-dom'
 
 import { Sidebar, UserProfile } from '../components/index'
+import Pins from './Pins'
+import { userQuery } from "../utils/data";
+import { client } from "../client"
+import logo from '../assets/logo.png'
+
 
 const Home = () => {
+
+    // State Hooks
+    const [user, setUser] = useState()
+    const [toggleSidebar, setToggleSidebar] = useState(false)
+    // Ref Hooks
+    const scrollRef = useRef(null)
+
+    // User Info
+    const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear()
+
+    // Effect Hooks
+    useEffect(() => {
+        const query = userQuery(userInfo?.googleId)
+
+        client.fetch(query).then((data) => {
+            setUser(data[0])
+        })
+    }, [])
+
+    useEffect(() => {
+        scrollRef.current.scrollTo(0, 0)
+    }, [])
+
     return (
-        <div>
-            Home
+        <div className='flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out'>
+            <div className='hidden md:flex h-screen flex-initial'>
+                <Sidebar user={user && user} />
+            </div>
+            <div className='flex md:hidden flex-row'>
+                <HiMenu fontSize={40} className='cursor-pointer' onClick={() => setToggleSidebar(true)} />
+                <Link to="/">
+                    <img src={logo} alt="logo" className='w-28' />
+                </Link>
+                <Link to={`user-profile/${user?.id}`}>
+                    <img src={user?.image} alt="logo" className='w-28' />
+                </Link>
+            </div>
+            {toggleSidebar && (
+                <div className='fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in'>
+                    <div className='absolute w-full flex justify-end items-center p-2'>
+                        <AiFillCloseCircle fontSize={30} className='cursor-pointer' onClick={() => setToggleSidebar(false)} />
+                    </div>
+                    <Sidebar user={user && user} closeToggle={setToggleSidebar} />
+                </div>
+            )}
+            <div className='pb-2 flex-1 h-screen overflow-y-scroll' ref={scrollRef}>
+
+            </div>
         </div>
     )
 }
